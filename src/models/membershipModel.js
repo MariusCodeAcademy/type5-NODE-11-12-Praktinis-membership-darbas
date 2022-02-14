549491556dab81a6f6cfca20eeffb75eb95fc835
Marius <marius@example.com>
2022-02-14 16:23:35 +0200

    creatting memberships

--- src/models/membershipModel.js
+++ src/models/membershipModel.js
@@ -1,12 +1,18 @@
-const dbClient = require('../db');
-const { getArrayFromDB } = require('./dbFunctions');
+const { createMembership } = require('../controllers/membershipsController');
+// const dbClient = require('../db');
+const { getArrayFromDB, createDocument } = require('./dbFunctions');
 const DB_NAME = 'cao-practice';
 const COLLECTION_NAME = 'memberships';
 
 async function getAllMembershipsDb() {
   return getArrayFromDB(DB_NAME, COLLECTION_NAME);
 }
+async function createMembershipDb(newMembershipData) {
+  if (!newMembershipData) throw new Error('nera newMembershipData');
+  return createDocument(DB_NAME, COLLECTION_NAME, newMembershipData);
+}
 
 module.exports = {
   getAllMembershipsDb,
+  createMembershipDb,
 };
