33d95c41254218ef433d7d64d40bf80a973f2eeb
Marius <marius@example.com>
2022-02-16 20:44:35 +0200

    aggregate users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -9,7 +9,30 @@ async function usersIndex(req, res) {
     failResponce(res);
     return;
   }
-  successResponce(res, allUsers);
+  // allUsers[0] ===
+  /*
+  {
+      "_id": "620ba13d143a7e1c0351075a",
+      "name": "James",
+      "surname": "Jameson",
+      "email": "james.jameson@gmail.com",
+      "membership": [
+        {
+          "_id": "620a5f791e6b8db1b3aa26a6",
+          "name": "Trial",
+          "price": 1.99,
+          "description": "This is 2 days membership"
+        }
+      ]
+    },
+  */
+
+  const usersWithMembershipName = allUsers.map((userObj) => ({
+    ...userObj,
+    membership: userObj.membership[0].name,
+  }));
+
+  successResponce(res, usersWithMembershipName);
 }
 async function createUser(req, res) {
   if (!req.body) throw new Error('nepaduoti duomenys i post createUser');
